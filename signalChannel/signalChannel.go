2490c11de1a177ac85c5b7afacb4cec5c45bfb07
mariapetrova.pmp <mariapetrova.pmp@gmail.com>
2024-10-18 23:04:46 +0300

    signalChannel

---
+++ signalChannel/signalChannel.go
@@ -0,0 +1,40 @@
+package signalChannel
+
+import (
+	"fmt"
+	"sync"
+)
+
+func SignalChannel() {
+	ch := make(chan int)
+	sig := make(chan struct{})
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case v, ok1 := <-ch:
+				if !ok1 {
+					fmt.Println("END")
+					return
+				}
+				fmt.Println(v)
+
+			case <-sig:
+				fmt.Println("DONE")
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		ch <- i
+	}
+	sig <- struct{}{}
+
+	close(ch)
+
+	wg.Wait()
+}
